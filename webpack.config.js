5926a20c76af1df6773b4f9f32fb28310bc29038
leny62 <lihirwe6@gmail.com>
2024-07-09 16:36:08 +0200

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,96 +1,109 @@
 const path = require("path");
 const webpack = require("webpack");
-const CopyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin');
-const fs = require('fs');
-
-const confPath = path.resolve(__dirname, 'conf');
-if (fs.existsSync(confPath)) {
-  require('custom-env').env(true, 'conf');
-}
+const HtmlWebpackPartialsPlugin = require("html-webpack-partials-plugin");
 
 module.exports = {
-  mode: "production", // Use production mode for deployment
-  output: {
-    publicPath: "/",
-    filename: "js/main.[contenthash].js",
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "css/main.[contenthash].css",
-    }),
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-    }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: 'src/favicons',
-          to: 'assets',
-        },
-      ],
-    }),
-    new HtmlWebpackPartialsPlugin({
-      path: path.resolve(__dirname, 'partials/analytics.html'),
-      location: 'head',
-      priority: 'high',
-      template_filename: '*',
-      options: {}
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        include: [path.resolve(__dirname, "src")],
-        loader: "babel-loader",
-      },
-      {
-        test: /.(sa|sc|c)ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: true,
-              importLoaders: 1,
-            },
-          },
-          {
-            loader: "postcss-loader",
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: true,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif|mp4|webm)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "assets/images/[hash][ext][query]", // Ensure images are placed in the assets/images directory
-        },
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "fonts/[hash][ext][query]",
-        },
-      },
-    ],
-  },
-  devServer: {
-    open: true,
-    host: "localhost",
-  },
+	mode: "development",
+	output: {
+		publicPath: "/",
+		filename: "main.[contenthash].js",
+	},
+	plugins: [
+		new webpack.ProgressPlugin(),
+		new MiniCssExtractPlugin({
+			filename: "main.[contenthash].css",
+		}),
+		new HtmlWebpackPlugin({
+			template: "src/index.html",
+		}),
+		new CopyPlugin({
+			patterns: [
+				{
+					from: "src/favicons",
+					to: "assets",
+				},
+			],
+		}),
+	],
+
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				include: [path.resolve(__dirname, "src")],
+				loader: "babel-loader",
+			},
+			{
+				test: /\.(pdf)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "[name].[ext]",
+						},
+					},
+				],
+			},
+			{
+				test: /.(sa|sc|c)ss$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					{
+						loader: "css-loader",
+						options: {
+							sourceMap: true,
+							importLoaders: 1,
+						},
+					},
+					{
+						loader: "postcss-loader",
+					},
+					{
+						loader: "sass-loader",
+						options: {
+							sourceMap: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+				type: "asset/resource",
+				generator: {
+					filename: "images/[hash][ext][query]",
+				},
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: "asset/resource",
+				generator: {
+					filename: "fonts/[name][ext]",
+				},
+			},
+		],
+	},
+
+	devServer: {
+		open: true,
+		host: "localhost",
+	},
+
+	optimization: {
+		moduleIds: "deterministic",
+		runtimeChunk: "single",
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: "vendors",
+					chunks: "all",
+				},
+			},
+		},
+	},
 };
